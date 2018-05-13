876c3f57b6b5943c6fd2e97dcbb5d084d8bf8b96
t-moko <tomoka.1104@me.com>
2018-05-13 16:24:37 +0900

    とりあえず繰り返し処理を諦めた物

--- js/scroll.js
+++ js/scroll.js
@@ -4,33 +4,27 @@ for(var i=0; i < v.length; i++){
   v[i].currentTime = 0;
 }
 
-var scrollElm = (function() {
-  if('scrollingElement' in document) return document.scrollingElement;
-  if(navigator.userAgent.indexOf('WebKit') != -1) return document.body;
-  return document.documentElement;
-})();
-(function() {
-  var duration = 500;
-  var ignore = '.noscroll';
-  var easing = function (t, b, c, d) { return c * (0.5 - Math.cos(t / d * Math.PI) / 2) + b; }; //jswing
-  var smoothScrollElm = document.querySelectorAll('a[href^="#"]:not(' + ignore +')');
-  Array.prototype.forEach.call(smoothScrollElm, function(elm) {
-    elm.addEventListener('click', function(e) {
-      e.preventDefault();
-      var targetElm = document.querySelector(elm.getAttribute('href'));
-      if(!targetElm) return;
-      var targetPos = targetElm.getBoundingClientRect().top;
-      var startTime = Date.now();
-      var scrollFrom = scrollElm.scrollTop;
-      (function loop() {
-        var currentTime = Date.now() - startTime;
-        if(currentTime < duration) {
-          scrollTo(0, easing(currentTime, scrollFrom, targetPos, duration));
-          window.requestAnimationFrame(loop);
-        } else {
-          scrollTo(0, targetPos + scrollFrom);
-        }
-      })();
-    })
-  });
-})();
+window.addEventListener('load', function(){
+	var navElm = document.querySelectorAll('nav > ul > li');
+	console.log(document.querySelector('#main'));
+	navElm[0].addEventListener('click', function(e) {
+		e.preventDefault();
+		document.querySelector('#main').scrollIntoView({behavior: 'smooth'});
+	});
+	navElm[1].addEventListener('click', function(e) {
+		e.preventDefault();
+		document.querySelector('#profile').scrollIntoView({behavior: 'smooth'});
+	});
+	navElm[2].addEventListener('click', function(e) {
+		e.preventDefault();
+		document.querySelector('#works').scrollIntoView({behavior: 'smooth'});
+	});
+	navElm[3].addEventListener('click', function(e) {
+		e.preventDefault();
+		document.querySelector('#link').scrollIntoView({behavior: 'smooth'});
+	});
+	navElm[4].addEventListener('click', function(e) {
+		e.preventDefault();
+		document.querySelector('#extra').scrollIntoView({behavior: 'smooth'});
+	});
+});
